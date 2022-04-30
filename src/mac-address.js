8c66d7e59e02ffb6f159a9802c8e0d75d918efc4
viktoriam01 <aislingdar@gmail.com>
2022-04-30 23:25:42 +0800

    feat: 10/21

--- src/mac-address.js
+++ src/mac-address.js
@@ -14,9 +14,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
+function isMAC48Address(n) {
   throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+//  let n = '00-1B-63-84-45-E6';
+ 
+//   let num = '0123456789'
+//   let char = 'ABCDEF'
+//   let hyphens = '-'
+
+//   let arr = n.split('')
+//   arr.forEach(char => {
+//     if(!num.includes(char) && !char.includes(char) && !hyphens.includes(char)) {
+//       return false
+//     } else {
+//     return true}
+
+//   })
+  
 }
 module.exports = {
   isMAC48Address
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -13,98 +13,48 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function sortByHeight(arr) {
  
-  throw new NotImplementedError('Not implemented');
+  // throw new NotImplementedError('Not implemented');
   // let arr = [-1, 150, 190, 170, -1, -1, 160, 180]
 
   // создаем мап значений с индексами
-// let mapped = arr.map(function(el, i) {
-// return { index: i, value: el };
-// });
-// console.log(mapped );
+  let mapped = arr.map(function(el, i) {
+  return { index: i, value: el };
+  });
+  
 
 // сортируем по возрастанию
-// let sort = mapped.sort(function(a, b) {
+let sort = mapped.sort(function(a, b) {
      
-//     if (a.value > b.value) {
-//       return 1; }
+    if (a.value > b.value) {
+      return 1; }
   
-//     if (a.value < b.value) {
-//       return -1; }
+    if (a.value < b.value) {
+      return -1; }
       
-//     return 0;
-//   });
-  
- 
-//  console.log(sort);
+    return 0;
+  });
+
 
 // Вриан 2 - разбивам на массив со значениями и массив с отсутствием значений
 
-// let numArr = []
-// let noneArr = []
+let numArr = []
+let noneArr = []
 
-// mapped.forEach(({index, value}) => {
-//    return value !==-1 ? numArr.push({index, value}) : noneArr.push({index, value});
-// });
-
-// console.log(numArr);
-// console.log(noneArr);
+mapped.forEach(({index, value}) => {
+   return value !==-1 ? numArr.push(value) : noneArr.push({index, value});
+});
 
 // воткнуть значения  noneArr в массив numArr по возрастаниюзначений индексов
-// let res =[]
-// noneArr.forEach(a => {
-//   numArr.forEach(b => {
-//     console.log("a", a.index);
-//     console.log("b",b.index);
-//     return res = (a.index === b.index - 1) ? [
-//       numArr.slice(0, b.index),
-//       a,
-//       numArr.slice(b.index)
-//     ] : numArr;
-//   })
+
+noneArr.forEach(a => {
+   numArr.splice(a.index, 0, a.value)
+  })
   
-// })
-// console.log(res);
+return numArr
+
 }
 module.exports = {
   sortByHeight
 };
 
 
-
-
-
-
-// другой вариант
-
-// arr.sort(function(a, b) {
-//     if (a === - 1 || b === -1)  return 0;
-//     if (a === - 1 && b === -1)  return 0; 
-//   if (a > b) {
-//     return 1; }
-
-//   if (a < b) {
-//     return -1; }
-    
-//   return 0;
-// });
-
-// console.log(arr);
-
- 
-
-// // Вриант 1 - через пузырек
-// for (let i = 0; i < sort.length - 1; i++) {
-//   // в рамках каждого прохода нам тоже нужно просматривать на один элемент меньше,
-//   // ведь для последнего элемента мы не найдем следующий, с которым будем его сравнивать
-//   // а ещё каждую итерацию будем смотреть на один элемент поменьше, ведь справа будет уже сортированная его часть
-//   for (let j = 0; j < sort.length - 1 - i; j++) {
-//     // если элемент слева больше элемента справа...
-//     if (sort[j].index.value > sort[j + 1].index.value) {
-//       /// ... меняем их местами
-//       [sort[j], sort[j + 1]] = [sort[j + 1], sort[j]];
-//     }
-//   }
-
-//   // logIteration(array);
-// }
-// // console.log(sort);
