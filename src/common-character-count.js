577165f7fc0d4e98f759bf9e6ec7c7abe8d4100d
viktoriam01 <aislingdar@gmail.com>
2022-04-30 22:38:08 +0800

    feat: 9/21

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,11 +11,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount( s1, s2 ) {
+
+let counter = 0;  
+let arr1 = s1.split("")
+let arr2 = s2.split('')
+
+  for(let char of arr1) {
+    if(arr2.indexOf(char) !== -1) {
+      let index = arr2.indexOf(char)
+      arr2.splice(index, 1)
+      counter +=1
+    }
+  }
+  return counter;
 }
 
 module.exports = {
   getCommonCharacterCount
 };
+
+
--- src/sort-by-height.js
+++ src/sort-by-height.js
@@ -14,13 +14,67 @@ const { NotImplementedError } = require('../extensions/index.js');
 function sortByHeight(arr) {
  
   throw new NotImplementedError('Not implemented');
+  // let arr = [-1, 150, 190, 170, -1, -1, 160, 180]
 
-// var mapped = arr.map(function(el, i) {
+  // создаем мап значений с индексами
+// let mapped = arr.map(function(el, i) {
 // return { index: i, value: el };
 // });
 // console.log(mapped );
 
+// сортируем по возрастанию
+// let sort = mapped.sort(function(a, b) {
+     
+//     if (a.value > b.value) {
+//       return 1; }
+  
+//     if (a.value < b.value) {
+//       return -1; }
+      
+//     return 0;
+//   });
+  
+ 
+//  console.log(sort);
 
+// Вриан 2 - разбивам на массив со значениями и массив с отсутствием значений
+
+// let numArr = []
+// let noneArr = []
+
+// mapped.forEach(({index, value}) => {
+//    return value !==-1 ? numArr.push({index, value}) : noneArr.push({index, value});
+// });
+
+// console.log(numArr);
+// console.log(noneArr);
+
+// воткнуть значения  noneArr в массив numArr по возрастаниюзначений индексов
+// let res =[]
+// noneArr.forEach(a => {
+//   numArr.forEach(b => {
+//     console.log("a", a.index);
+//     console.log("b",b.index);
+//     return res = (a.index === b.index - 1) ? [
+//       numArr.slice(0, b.index),
+//       a,
+//       numArr.slice(b.index)
+//     ] : numArr;
+//   })
+  
+// })
+// console.log(res);
+}
+module.exports = {
+  sortByHeight
+};
+
+
+
+
+
+
+// другой вариант
 
 // arr.sort(function(a, b) {
 //     if (a === - 1 || b === -1)  return 0;
@@ -36,46 +90,21 @@ function sortByHeight(arr) {
 
 // console.log(arr);
 
-// let numArr = []
-// let noneArr = []
+ 
 
-// mapped.forEach(({index, value}) => {
-//   console.log(value);
-//   return value !==-1 ? numArr.push({index, value}) : noneArr.push({index, value});
-// });
+// // Вриант 1 - через пузырек
+// for (let i = 0; i < sort.length - 1; i++) {
+//   // в рамках каждого прохода нам тоже нужно просматривать на один элемент меньше,
+//   // ведь для последнего элемента мы не найдем следующий, с которым будем его сравнивать
+//   // а ещё каждую итерацию будем смотреть на один элемент поменьше, ведь справа будет уже сортированная его часть
+//   for (let j = 0; j < sort.length - 1 - i; j++) {
+//     // если элемент слева больше элемента справа...
+//     if (sort[j].index.value > sort[j + 1].index.value) {
+//       /// ... меняем их местами
+//       [sort[j], sort[j + 1]] = [sort[j + 1], sort[j]];
+//     }
+//   }
 
-// console.log(numArr);
-// console.log(noneArr);
-
-
-  
-
-
-
-// noneArr.forEach(a => {
-//   numArr.forEach(b => {
-    
-//     return a.index === b.index - 1 ? [
-//       ...numArr.slice(0, b.index),
-//       a,
-//       ...numArr.slice(b.index)
-//     ] : numArr;
-//   })
-
-// let sort = mapped.sort(function(a, b) {
-//   console.log(a.index.value);
-//   if (a.index.value > b.index.value) {
-//     return 1; }
-//   if (a.index.value < b.index.value) {
-//     return -1; }
-//   return 0;
-// });
-// console.log(sort);
-
-
-
-}
-
-module.exports = {
-  sortByHeight
-};
+//   // logIteration(array);
+// }
+// // console.log(sort);
